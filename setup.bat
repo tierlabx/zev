52a133dc4521d644b5520139541a7c47a2b2327e
tierlabx <1765080897zh@gmail.com>
2026-08-13 15:11:19 +0800

    style: 增加git  hook

---
+++ setup.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Configuring git hooks for ZEV project...
+git config core.hooksPath .githooks
+echo Git hooks successfully configured!
+pause
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Configuring git hooks for ZEV project..."
+git config core.hooksPath .githooks
+echo "Git hooks successfully configured!"
